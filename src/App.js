c18633988ed9fee0f9cd2c3f41f2afbec9d49d4d
aam-himel <aam.himel@gmail.com>
2022-05-18 12:29:09 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
